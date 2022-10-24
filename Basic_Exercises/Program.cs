d89028e9fbcbc67948139e91968b519bb7ff6fb5
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-24 19:42:24 +0800

    #6: program to print the output of multiplication of three numbers which will be entered by the user

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -10,15 +10,18 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            int num1, num2;
+            int[] number = new int[3];
+            int total;
 
-            Console.Write("Input the first number: ");
-            num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the second number: ");
-            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the first number to multiply: ");
+            number[0] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the second number to multiply: ");
+            number[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input the third number to multiply: ");
+            number[2] = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"\nFirst Number: {num2}");
-            Console.WriteLine($"Second Number: {num1}");
+            total = number[0] * number[1] * number[2];
+            Console.WriteLine($"\n{number[0]} x {number[1]} x {number[2]} = {total}");
         }
     }
 }
