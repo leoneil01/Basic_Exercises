f37d4e66086e7fcc3df29a6621e63aec90a34e23
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-24 19:58:54 +0800

    #7: program to print on screen the output of adding, subtracting, multiplying and dividing of two numbers which will be entered by the user

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -10,18 +10,25 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            int[] number = new int[3];
-            int total;
+            int[] total = new int[5];
+            int[] number = new int[2];
 
-            Console.Write("Input the first number to multiply: ");
+            Console.Write("Input the first number: ");
             number[0] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the second number to multiply: ");
+            Console.Write("Input the second number: ");
             number[1] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the third number to multiply: ");
-            number[2] = Convert.ToInt32(Console.ReadLine());
 
-            total = number[0] * number[1] * number[2];
-            Console.WriteLine($"\n{number[0]} x {number[1]} x {number[2]} = {total}");
+            total[0] = number[0] + number[1];
+            total[1] = number[0] - number[1];
+            total[2] = number[0] * number[1];
+            total[3] = number[0] / number[1];
+            total[4] = number[0] % number[1];
+
+            Console.WriteLine($"\n{number[0]} + {number[1]} = {total[0]}");
+            Console.WriteLine($"{number[0]} - {number[1]} = {total[1]}");
+            Console.WriteLine($"{number[0]} x {number[1]} = {total[2]}");
+            Console.WriteLine($"{number[0]} / {number[1]} = {total[3]}");
+            Console.WriteLine($"{number[0]} mod {number[1]} = {total[4]}");
         }
     }
 }
