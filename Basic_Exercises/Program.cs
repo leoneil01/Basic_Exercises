54a94c12921a338e775d1eb69348c7d27ea909d7
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-25 07:49:12 +0800

    #11: program that takes an age (for example 20) as input and prints something as (You look older than 20)

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -10,19 +10,12 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            int x, y, z;
-            int[] total = new int[2];
+            int age;
 
-            Console.Write("Enter first number: ");
-            x = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter second number: ");
-            y = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter third number: ");
-            z = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter your age: ");
+            age = Convert.ToInt32(Console.ReadLine());
 
-            total[0] = (x + y) * z;
-            total[1] = (x * y) + (y * z);
-            Console.WriteLine($"\nResult of specified numbers {x}, {y} and {z}, (x + y).z is {total[0]} and x.y + y.z is {total[1]}");
+            Console.WriteLine($"\nYou look older than {age}");
         }
     }
 }
