34da824db614769fbf0b9edbcc9dffd87829de3b
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-24 20:20:29 +0800

    #8: program that takes a number as input and print its multiplication table

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -10,25 +10,35 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            int[] total = new int[5];
-            int[] number = new int[2];
+            int[] total = new int[11];
+            int number;
 
-            Console.Write("Input the first number: ");
-            number[0] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the second number: ");
-            number[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the number: ");
+            number = Convert.ToInt32(Console.ReadLine());
 
-            total[0] = number[0] + number[1];
-            total[1] = number[0] - number[1];
-            total[2] = number[0] * number[1];
-            total[3] = number[0] / number[1];
-            total[4] = number[0] % number[1];
+            total[0] = number * 0;
+            total[1] = number * 1;
+            total[2] = number * 2;
+            total[3] = number * 3;
+            total[4] = number * 4;
+            total[5] = number * 5;
+            total[6] = number * 6;
+            total[7] = number * 7;
+            total[8] = number * 8;
+            total[9] = number * 9;
+            total[10] = number * 10;
 
-            Console.WriteLine($"\n{number[0]} + {number[1]} = {total[0]}");
-            Console.WriteLine($"{number[0]} - {number[1]} = {total[1]}");
-            Console.WriteLine($"{number[0]} x {number[1]} = {total[2]}");
-            Console.WriteLine($"{number[0]} / {number[1]} = {total[3]}");
-            Console.WriteLine($"{number[0]} mod {number[1]} = {total[4]}");
+            Console.WriteLine($"\n{number} * 0 = {total[0]}");
+            Console.WriteLine($"{number} * 1 = {total[1]}");
+            Console.WriteLine($"{number} * 2 = {total[2]}");
+            Console.WriteLine($"{number} * 3 = {total[3]}");
+            Console.WriteLine($"{number} * 4 = {total[4]}");
+            Console.WriteLine($"{number} * 5 = {total[5]}");
+            Console.WriteLine($"{number} * 6 = {total[6]}");
+            Console.WriteLine($"{number} * 7 = {total[7]}");
+            Console.WriteLine($"{number} * 8 = {total[8]}");
+            Console.WriteLine($"{number} * 9 = {total[9]}");
+            Console.WriteLine($"{number} * 10 = {total[10]}");
         }
     }
 }
