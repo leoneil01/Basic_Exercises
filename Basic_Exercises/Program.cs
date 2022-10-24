45183d627c0d986ac7c28258ab13e91169b1ad7f
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-25 07:24:16 +0800

    #9: program that takes four numbers as input to calculate and print the average

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -10,35 +10,20 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            int[] total = new int[11];
-            int number;
+            double[] number = new double[4];
+            double avg;
 
-            Console.Write("Enter the number: ");
-            number = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the first number: ");
+            number[0] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the second number: ");
+            number[1] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the third number: ");
+            number[2] = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter the fourth number: ");
+            number[3] = Convert.ToDouble(Console.ReadLine());
 
-            total[0] = number * 0;
-            total[1] = number * 1;
-            total[2] = number * 2;
-            total[3] = number * 3;
-            total[4] = number * 4;
-            total[5] = number * 5;
-            total[6] = number * 6;
-            total[7] = number * 7;
-            total[8] = number * 8;
-            total[9] = number * 9;
-            total[10] = number * 10;
-
-            Console.WriteLine($"\n{number} * 0 = {total[0]}");
-            Console.WriteLine($"{number} * 1 = {total[1]}");
-            Console.WriteLine($"{number} * 2 = {total[2]}");
-            Console.WriteLine($"{number} * 3 = {total[3]}");
-            Console.WriteLine($"{number} * 4 = {total[4]}");
-            Console.WriteLine($"{number} * 5 = {total[5]}");
-            Console.WriteLine($"{number} * 6 = {total[6]}");
-            Console.WriteLine($"{number} * 7 = {total[7]}");
-            Console.WriteLine($"{number} * 8 = {total[8]}");
-            Console.WriteLine($"{number} * 9 = {total[9]}");
-            Console.WriteLine($"{number} * 10 = {total[10]}");
+            avg = (number[0] + number[1] + number[2] + number[3]) / 4;
+            Console.WriteLine($"\nThe average of {number[0]}, {number[1]}, {number[2]}, {number[3]} is {avg}");
         }
     }
 }
