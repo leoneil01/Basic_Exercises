37a75a7753d514bf1715cb931e97970db58a150e
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-26 05:37:28 +0800

    #26: program that compute the sum of the first 500 prime numbers

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -11,14 +11,40 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Odd numbers from 1 to 99: \n");
-            for (int n = 1; n < (99 + 1); n++)
-            {
-                if (n % 2 != 0)
-                {
-                    Console.WriteLine(n);
-                }
-            }
+        //    long sum;
+        //    int ctr;
+        //    int n;
+
+        //    Console.WriteLine("Sum of the first 500 prime numbers:");
+        //    sum = 0;
+        //    ctr = 0;
+        //    n = 2;
+        //    while (ctr < 500)
+        //    {
+        //        if (isPrime(n))
+        //        {
+        //            sum += n;
+        //            ctr++;
+        //        }
+        //        n++;
+        //    }
+
+        //    Console.WriteLine(sum.ToString());
+        //}
+
+        //public static bool isPrime (int n)
+        //{
+        //    int x = (int) Math.Floor(Math.Sqrt(n));
+
+        //    if (n == 1) return false;
+        //    if (n == 2) return true;
+
+        //    for (int i = 2; i <= x; i++)
+        //    {
+        //        if (n % i == 0) return false;
+        //    }
+
+        //    return true;
         }
     }
 }
