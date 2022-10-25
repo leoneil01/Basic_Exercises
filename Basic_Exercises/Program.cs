48222c1e66a5b82d17b1885c0d45ec4af72e2ce9
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-10-25 17:55:56 +0800

    #22: program that check if a given integer is within 20 to 100, or 200 will be false

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -11,21 +11,13 @@ namespace Basic_Exercises
     {
         static void Main(string[] args)
         {
-            int[] number = new int[2];
+            int num1;
 
-            Console.Write("Input the first number: ");
-            number[0] = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input the second number: ");
-            number[1] = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter a number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
 
-            if (number[0] == 20 || number[1] == 20 || (number[0] + number[1] == 20))
+            if (num1 >= 20 && num1 <= 100 || num1 == 200)
             {
-                Console.WriteLine($"\n{number[0]} + {number[1]} = {number[0] + number[1]}");
-                Console.WriteLine("True");
-            }
-            else
-            {
-                Console.WriteLine($"\n{number[0]} + {number[1]} = {number[0] + number[1]}");
                 Console.WriteLine("False");
             }
         }
