091e72b0e400ddc00007efc377641fe19ccb448c
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-22 20:39:14 +0800

    #43: program that check if a given string starts with "w" and immediately followed by two "ww", return false

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -11,12 +11,17 @@ namespace Basic_Exercises
 {
     class Program
     {
-        static void Main(string[] args)
-        {
 		    Console.Write("Input a string: ");
 		    string str = Console.ReadLine();
 		    
-		    Console.WriteLine(str.Length < 4 ? str.ToUpper() : str.Substring(0, 4).ToLower() + str.Substring(4, str.Length - 4));
+		    // takes the first three indexes and if it's three w (www), will print false
+		    Console.WriteLine(str.Length > 2 ? (str.Substring(0, 1).Equals("w") && str.Substring(1, 2).Equals("ww") ? false : true) : true); // ?bug
+		    
+		    // if(str.Length > 2) {
+		    //     Console.WriteLine(str.Substring(0, 1).Equals("w") && str.Substring(1, 2).Equals("ww") ? false : true);
+		    // } else {
+		    //     Console.WriteLine(true);
+		    // }
         }
     }
 }
