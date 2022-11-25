1161fe48118adf94067af0bc5906a9d8dabd2e8e
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-25 19:45:19 +0800

    #48: program that check if the first element and the last element are equal of an array of integers and length is 1 or more

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -11,14 +11,9 @@ namespace Basic_Exercises
 {
     class Program
     {
-        int[] array = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1
-        };
+        int[] array = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
         Console.WriteLine("Array: {0}", string.Join(", ", array));
         
-        int total = 0;
-        for(int i = 0; i < array.Length; i++) {
-            total += array[i];
-        }
-        Console.WriteLine("Sum: {0}", total);
+        Console.WriteLine(array[0] == array[array.Length - 1] && array.Length >= 1);
     }
 }
