899350a579409b89ffb0c7984ad3f0726afe1d89
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-26 12:20:06 +0800

    #49: program that check if the first element or the last element of the two arrays (length is more than 1) are equal

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -11,9 +11,14 @@ namespace Basic_Exercises
 {
     class Program
     {
-        int[] array = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
-        Console.WriteLine("Array: {0}", string.Join(", ", array));
+        int[] array1 = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1};
+        int[] array2 = {1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 5};
+        Console.WriteLine("Array1: [{0}]", string.Join(", ", array1));
+        Console.WriteLine("Array2: [{0}]", string.Join(", ", array2));
         
-        Console.WriteLine(array[0] == array[array.Length - 1] && array.Length >= 1);
+        Console.WriteLine("---------------------------------------------------------------------");
+        Console.WriteLine("Check if the first and last element of the two arrays (length is more than 1) is equal.");
+        
+        Console.WriteLine((array1[0] == array2[0] || array1[array1.Length - 1] == array2[array2.Length - 1]) && (array1.Length > 1 && array2.Length > 1));
     }
 }
