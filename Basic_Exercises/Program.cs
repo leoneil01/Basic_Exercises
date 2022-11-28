1d11f1f4fe7f4f3f74f468b3f5feca7bd927269b
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-28 21:15:42 +0800

    #57: program that find the pair of adjacent elements that has the highest product of a given array of integers

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,21 +12,31 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program palindrome = new Program();
+		    // Program adjacentElementProduct = new Program();
 		    
-		    // Console.WriteLine("Check if a given string is a palindrome or not");
-		    // Console.WriteLine("\n--------------------------------------------------\n");
+		    // int[] array1 = {1, -3, 4, -5, 1};
+		    // int[] array2 = {1, 3, 4, 5, 2};
+		    // int[] array3 = {1, 3, -4, 5, 2};
+		    // int[] array4 = {1, 0, 4, 0, 2};
+		    // Console.WriteLine("Array1: [{0}]", string.Join(", ", array1));
+		    // Console.WriteLine("Array2: [{0}]", string.Join(", ", array2));
+		    // Console.WriteLine("Array3: [{0}]", string.Join(", ", array3));
+		    // Console.WriteLine("Array4: [{0}]", string.Join(", ", array4));
 		    
-		    // Console.Write("Please input a string: ");
-		    // string strInput = Console.ReadLine();
+		    // Console.WriteLine("\n----------------------------\n");
 		    
-		    // Console.WriteLine(palindrome.CheckPalindrome(strInput));
+		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array1));
+		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array2));
+		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array3));
+		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array4));
 		}
 		
-		// public bool CheckPalindrome(string str) {
-		//     char[] charArray = str.ToCharArray();
-		//     Array.Reverse(charArray);
-		//     return new string(charArray).Equals(str);
+		// public int AdjacentElementProduct(int[] inputArray) {
+		//     int max = inputArray[0] * inputArray[1];
+		//     for(int i = 1; i <= inputArray.Length - 2; i++) {
+		//         max = Math.Max(max, inputArray[i] * inputArray[i + 1]);
+		//     }
+		//     return max;
 		// }
     }
 }
