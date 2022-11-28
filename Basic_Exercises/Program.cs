c81b5b7950547f82e90ba2b51080754f7bae2330
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-28 20:43:58 +0800

    #56: program that check if a given string is a palindrome or not

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,24 +12,21 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program adjacentElementProduct = new Program();
+		    // Program palindrome = new Program();
 		    
-		    // Console.WriteLine(adjacentElementProduct.ArrayAdjacentElementsProduct(new int[] {2, 4, 2, 6, 9, 3}) == 27);
-		    // Console.WriteLine(adjacentElementProduct.ArrayAdjacentElementsProduct(new int[] {0, -1, -1, -2}) == 2);
-		    // Console.WriteLine(adjacentElementProduct.ArrayAdjacentElementsProduct(new int[] {6, 1, 12, 3, 1, 4}) == 36);
-		    // Console.WriteLine(adjacentElementProduct.ArrayAdjacentElementsProduct(new int[] {1, 4, 3, 0}) == 16);
+		    // Console.WriteLine("Check if a given string is a palindrome or not");
+		    // Console.WriteLine("\n--------------------------------------------------\n");
+		    
+		    // Console.Write("Please input a string: ");
+		    // string strInput = Console.ReadLine();
+		    
+		    // Console.WriteLine(palindrome.CheckPalindrome(strInput));
 		}
 		
-		// public int ArrayAdjacentElementsProduct(int[] inputArray) {
-		//     int arrayIndex = 0;
-		//     int product = inputArray[arrayIndex] * inputArray[arrayIndex + 1];
-		    
-		//     arrayIndex++;
-		//     while(arrayIndex + 1 < inputArray.Length) {
-		//         product = (inputArray[arrayIndex] * inputArray[arrayIndex + 1]) > product ? inputArray[arrayIndex] * inputArray[arrayIndex + 1] : product;
-		//         arrayIndex++;
-		//     }
-		//     return product;
+		// public bool CheckPalindrome(string str) {
+		//     char[] charArray = str.ToCharArray();
+		//     Array.Reverse(charArray);
+		//     return new string(charArray).Equals(str);
 		// }
     }
 }
