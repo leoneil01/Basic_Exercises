0cf8ff98650965b728388c118c308e8a28f6be65
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-29 05:24:39 +0800

    #58: program which will accept a list of integers and checks how many integers are needed to complete the range

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,31 +12,26 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program adjacentElementProduct = new Program();
+		    // Program consecutiveArray = new Program();
 		    
-		    // int[] array1 = {1, -3, 4, -5, 1};
-		    // int[] array2 = {1, 3, 4, 5, 2};
-		    // int[] array3 = {1, 3, -4, 5, 2};
-		    // int[] array4 = {1, 0, 4, 0, 2};
+		    // int[] array1 = {1, 3, 5, 6, 9};
+		    // int[] array2 = {0, 10};
 		    // Console.WriteLine("Array1: [{0}]", string.Join(", ", array1));
 		    // Console.WriteLine("Array2: [{0}]", string.Join(", ", array2));
-		    // Console.WriteLine("Array3: [{0}]", string.Join(", ", array3));
-		    // Console.WriteLine("Array4: [{0}]", string.Join(", ", array4));
 		    
-		    // Console.WriteLine("\n----------------------------\n");
+		    // Console.WriteLine("-------------------------");
 		    
-		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array1));
-		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array2));
-		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array3));
-		    // Console.WriteLine(adjacentElementProduct.AdjacentElementProduct(array4));
+		    // Console.WriteLine(consecutiveArray.ConsecutiveArray(array1));
+		    // Console.WriteLine(consecutiveArray.ConsecutiveArray(array2));
 		}
 		
-		// public int AdjacentElementProduct(int[] inputArray) {
-		//     int max = inputArray[0] * inputArray[1];
-		//     for(int i = 1; i <= inputArray.Length - 2; i++) {
-		//         max = Math.Max(max, inputArray[i] * inputArray[i + 1]);
+		// public int ConsecutiveArray(int[] inputArray) {
+		//     Array.Sort(inputArray);
+		//     int ctr = 0;
+		//     for(int i = 0; i < inputArray.Length - 1; i++) {
+		//         ctr += inputArray[i + 1] - inputArray[i] - 1;
 		//     }
-		//     return max;
+		//     return ctr;
 		// }
     }
 }
