5119ee6498077bf20fb42b990bc62860ac36055d
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-11-29 05:42:19 +0800

    #59: program that check whether it is possible to create a strictly increasing sequence from a given sequence of integers as an array

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,26 +12,31 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program consecutiveArray = new Program();
+		    // Program increasingSequence = new Program();
 		    
 		    // int[] array1 = {1, 3, 5, 6, 9};
 		    // int[] array2 = {0, 10};
+		    // int[] array3 = {1, 3, 1, 3};
 		    // Console.WriteLine("Array1: [{0}]", string.Join(", ", array1));
 		    // Console.WriteLine("Array2: [{0}]", string.Join(", ", array2));
+		    // Console.WriteLine("Array3: [{0}]", string.Join(", ", array3));
 		    
 		    // Console.WriteLine("-------------------------");
 		    
-		    // Console.WriteLine(consecutiveArray.ConsecutiveArray(array1));
-		    // Console.WriteLine(consecutiveArray.ConsecutiveArray(array2));
+		    // Console.WriteLine(increasingSequence.IncreasingSequence(array1));
+		    // Console.WriteLine(increasingSequence.IncreasingSequence(array2));
+		    // Console.WriteLine(increasingSequence.IncreasingSequence(array3));
 		}
 		
-		// public int ConsecutiveArray(int[] inputArray) {
-		//     Array.Sort(inputArray);
+		// public bool IncreasingSequence(int[] inputArray) {
 		//     int ctr = 0;
 		//     for(int i = 0; i < inputArray.Length - 1; i++) {
-		//         ctr += inputArray[i + 1] - inputArray[i] - 1;
+		//         if(inputArray[i] >= inputArray[i + 1])
+		//             ctr++;
+		//         if(i + 2 < inputArray.Length && inputArray[i] >= inputArray[i +2])
+		//             ctr++;
 		//     }
-		//     return ctr;
+		//     return ctr <= 2;
 		// }
     }
 }
