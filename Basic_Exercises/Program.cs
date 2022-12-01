8ce7afd90b975b48808cd0be126ad3f371574fbf
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-01 20:53:51 +0800

    #62: program that reverse the strings contained in each pair of matching parentheses in a given string and also remove the parentheses within the given string

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,21 +12,32 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program sortNum = new Program();
+		    // Program rrp = new Program();
 		    
-		    // int[] x = sortNum.SortNumbers(new int[] {-5, 236, 120, 70, -5, -5, 698, 280});
-		    // Console.WriteLine("Array: [{0}]", string.Join(", ", x));
-		    // Console.WriteLine("Sorted Array: ");
-		    // foreach(int item in x) {
-		    //     Console.WriteLine(item.ToString());
-		    // }
+		    // Console.WriteLine("Reverse the strings contained in each pair of matching parentheses in a given string");
+		    // Console.WriteLine("\n--------------------------------------------------------------------------------------\n");
+		    
+		    // Console.WriteLine("String1: p(rq)st");
+		    // Console.WriteLine("Reverse String1: {0}", rrp.ReverseRemoveParentheses("p(rq)st"));
+		    // Console.WriteLine("String2: (p(rq)st)");
+		    // Console.WriteLine("Reverse String2: {0}", rrp.ReverseRemoveParentheses("(p(rq)st)"));
+		    // Console.WriteLine("String3: ab(cd(ef)gh)ij)");
+		    // Console.WriteLine("Reverse String3: {0}", rrp.ReverseRemoveParentheses("ab(cd(ef)gh)ij"));
 		}
-		
-		// public int[] SortNumbers(int[] array) {
-		//     int[] numbers = array.Where(x => x != -5).OrderBy(x => x).ToArray();
-		//     int ctr = 0;
-		    
-		//     return array.Select(x => x >= 0 ? numbers[ctr++] : -5).ToArray();
+    
+		// public string ReverseRemoveParentheses(string str) {
+		//     int lid = str.LastIndexOf('(');
+		//     if(lid == -1) {
+		//         return str;
+		//     } else {
+		//         int rid = str.IndexOf(')', lid);
+		        
+		//         return ReverseRemoveParentheses(
+		//             str.Substring(0, lid)
+		//             + new string(str.Substring(lid + 1, rid - lid -1).Reverse().ToArray())
+		//             + str.Substring(rid + 1)
+		//         );
+		//     }
 		// }
     }
 }
