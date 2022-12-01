f192c19218fbe0259d33363a4ce692befb489c35
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-01 20:05:29 +0800

    #61: program that sort the integers in ascending order without moving the number -5

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,32 +12,21 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program sumMatrix = new Program();
+		    // Program sortNum = new Program();
 		    
-		    // Console.WriteLine(sumMatrix.SumMatrixElements(new int[][] {
-		    //     new int[] {0, 2, 3, 2},
-		    //     new int[] {0, 6, 0, 1},
-		    //     new int[] {4, 0, 3, 0}
-		    // }));
-		    // Console.WriteLine(sumMatrix.SumMatrixElements(new int[][] {
-		    //     new int[] {1, 2, 1, 0},
-		    //     new int[] {0, 5, 0, 0},
-		    //     new int[] {1, 1, 3, 10}
-		    // }));
-		    // Console.WriteLine(sumMatrix.SumMatrixElements(new int[][] {
-		    //     new int[] {1, 1},
-		    //     new int[] {2, 2},
-		    //     new int[] {3, 3},
-		    //     new int[] {4, 4}
-		    // }));
+		    // int[] x = sortNum.SortNumbers(new int[] {-5, 236, 120, 70, -5, -5, 698, 280});
+		    // Console.WriteLine("Array: [{0}]", string.Join(", ", x));
+		    // Console.WriteLine("Sorted Array: ");
+		    // foreach(int item in x) {
+		    //     Console.WriteLine(item.ToString());
+		    // }
 		}
 		
-		// public int SumMatrixElements(int[][] matrix) {
+		// public int[] SortNumbers(int[] array) {
+		//     int[] numbers = array.Where(x => x != -5).OrderBy(x => x).ToArray();
 		//     int ctr = 0;
-		//     for(int i = 0; i < matrix[0].Length; i++)
-		//         for(int j = 0; j < matrix.Length && matrix[j][i] > 0; j++)
-		//             ctr += matrix[j][i];
-		//     return ctr;
+		    
+		//     return array.Select(x => x >= 0 ? numbers[ctr++] : -5).ToArray();
 		// }
     }
 }
