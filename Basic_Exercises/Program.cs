6d87d0689df4bba48a292dd64ff32f74adc83e59
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-06 05:11:51 +0800

    #71: program that check if a given string contains two similar consecutive leters

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -14,17 +14,20 @@ namespace Basic_Exercises
     	public static void Main(string[] args) {
 		    Program program = new Program();
 		    
-		    Console.WriteLine("Remove the first and last elements from a given string");
-		    Console.WriteLine("-------------------------------------------------------");
-		    
 		    Console.Write("Please enter a string: ");
 		    string input = Console.ReadLine();
 		    
-		    Console.WriteLine("After removing first and last elements: {0}", program.Test(input));
+		    Console.WriteLine("-------------------------------------------");
+		    Console.WriteLine("Original String: {0}", input);
+		    Console.WriteLine("Test for consecutive similar letter: {0}", program.Test(input));
 		}
 		
-		public string Test(string str) {
-		    return str.Length > 1 ? str.Substring(1, str.Length - 2) : str;
+		public bool Test(string str) {
+		    for(int i = 0; i < str.Length - 1; i++) {
+		        if(str[i] == str[i + 1])
+		            return true;
+		    }
+		    return false;
 		}
     }
 }
