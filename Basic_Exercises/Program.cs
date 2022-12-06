d6559361fc2f9c83091db8432ae71c1a92fd3d9b
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-07 05:48:46 +0800

    #74: program that check the length of a given string is odd or even. Return Odd if the string length is odd otherwise Even

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,19 +12,22 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    // Program program = new Program();
+		    Program program = new Program();
 		    
-		    // Console.Write("Please enter a string: ");
-		    // string input = Console.ReadLine();
+		    Console.WriteLine("Check the length of a given string is odd or even\n");
+		    Console.Write("Please enter a string: ");
+		    string input = Console.ReadLine();
 		    
-		    // Console.WriteLine("------------------------------------------------------------------------");
+		    Console.WriteLine("------------------------------------------------------------------------");
 		    
-		    // Console.WriteLine("Original String: {0}", input);
-		    // Console.WriteLine("Convert the letters of the said string into alphabetical order: {0}", program.Test(input));
+		    Console.WriteLine("String: {0}", input);
+		    Console.WriteLine("String Length: {0}", program.Test(input));
 		}
 		
-		// public string Test(string str) {
-		//     return new string(str.OrderBy(x => x).ToArray());
-		// }
+		public string Test(string str) {
+		    if(str.Length % 2 == 1)
+		        return "Odd";
+		    return "Even";
+		}
     }
 }
