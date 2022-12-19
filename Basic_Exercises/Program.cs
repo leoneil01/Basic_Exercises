212a83ca1a0b8d0ff491a48ecac7c25bdf965338
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-20 05:03:49 +0800

    #79: program that convert an integer to string and a string to an integer

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,35 +12,26 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-<<<<<<< HEAD
 		    Program program = new Program();
 		    
-		    int[] nums1 = { 1, 2, 3 };
-		    int[] nums2 = { -2, 0, 3, 4 };
+		    string val1 = "50";
+		    Console.WriteLine("Original value and type: {0}, {1}", val1, val1.GetType());
+		    int result1 = program.StrToInt(val1);
+		    Console.WriteLine("----Comvert string to integer----");
+		    Console.WriteLine("Return value and type: {0}, {1}\n", result1, result1.GetType());
 		    
-		    Console.WriteLine("Array1: [{0}]", string.Join(", ", nums1));
-		    Console.WriteLine("Sum of squares of elements of the said array: {0}", program.Test(nums1));
-		    Console.WriteLine("\nArray2: [{0}]", string.Join(", ", nums2));
-		    Console.WriteLine("Sum of squares of elements of the said array: {0}", program.Test(nums2));
+		    int val2 = 122;
+		    Console.WriteLine("Original value and type: {0}, {1}", val2, val2.GetType());
+		    string result2 = program.IntToStr(val2);
+		    Console.WriteLine("----Comvert string to integer----");
+		    Console.WriteLine("Return value and type: {0}, {1}", result2, result2.GetType());
 		}
 		
-		public int Test(int[] nums) {
-		    return nums.Sum(n => n * n);
-=======
-			Program program = new Program();
-			
-			Console.WriteLine("Check whether a given word is plural or not");
-			Console.WriteLine("------------------------------------------");
-			
-			Console.Write("Please enter a string: ");
-			string input = Console.ReadLine();
-			
-			Console.WriteLine("Is '{0}' is plural? {1}", input, program.Test(input));
+		public int StrToInt(string str) {
+		    return int.Parse(str);
 		}
-		
-		public bool Test(string str) {
-			return str.EndsWith("s"); //* plural with 's' only, this doesn't include irregular plural like foot to feet
->>>>>>> 2a0e8ace6b1f8ca75234f0cec9c493a9c50ed76e
+		public string IntToStr(int i) {
+		    return i.ToString();
 		}
     }
 }
