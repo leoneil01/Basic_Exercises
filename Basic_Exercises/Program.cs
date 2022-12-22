12e44215a277afc66f49ca9dad3c774ffad8b467
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-23 06:20:25 +0800

    #80: program that convert all the values of a given array of mixed value to string values

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,26 +12,29 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-		    Program program = new Program();
-		    
-		    string val1 = "50";
-		    Console.WriteLine("Original value and type: {0}, {1}", val1, val1.GetType());
-		    int result1 = program.StrToInt(val1);
-		    Console.WriteLine("----Comvert string to integer----");
-		    Console.WriteLine("Return value and type: {0}, {1}\n", result1, result1.GetType());
-		    
-		    int val2 = 122;
-		    Console.WriteLine("Original value and type: {0}, {1}", val2, val2.GetType());
-		    string result2 = program.IntToStr(val2);
-		    Console.WriteLine("----Comvert string to integer----");
-		    Console.WriteLine("Return value and type: {0}, {1}", result2, result2.GetType());
+			// Program program = new Program();
+			
+			// object[] mixedArray = new object[5];
+			// mixedArray[0] = 25;
+			// mixedArray[1] = "Ann";
+			// mixedArray[2] = false;
+			// mixedArray[3] = DateTime.Now;
+			// mixedArray[4] = 112.22;
+			
+			// Console.WriteLine("---Printing array elements and their types---");
+			// for(int i = 0; i < mixedArray.Length; i++) {
+			//     Console.WriteLine("Value-> {0} :: Type-> {1}", mixedArray[i], mixedArray[i].GetType());
+			// }
+			
+			// string[] newNums = program.Test(mixedArray);
+			// Console.WriteLine("\n---Printing array elements and their types---");
+			// for(int i = 0; i < newNums.Length; i++) {
+			//     Console.WriteLine("Value-> {0} :: Type-> {1}", newNums[i], newNums[i].GetType());
+			// }
 		}
 		
-		public int StrToInt(string str) {
-		    return int.Parse(str);
-		}
-		public string IntToStr(int i) {
-		    return i.ToString();
-		}
+		// public string[] Test(object[] nums) {
+		//     return Array.ConvertAll(nums, x => x.ToString());
+		// }
     }
 }
