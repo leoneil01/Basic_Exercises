9a5c74e1725e9bebfafbc02914aaaba333d88124
Leoneil Odrunia <leoneilodrunia0121@gmail.com>
2022-12-26 20:42:44 +0800

    #81: program that swap a two digit given number and check whether the given number is greater than its swap value

--- Basic_Exercises/Program.cs
+++ Basic_Exercises/Program.cs
@@ -12,29 +12,15 @@ namespace Basic_Exercises
     class Program
     {
     	public static void Main(string[] args) {
-			// Program program = new Program();
+		// 	Program program = new Program();
 			
-			// object[] mixedArray = new object[5];
-			// mixedArray[0] = 25;
-			// mixedArray[1] = "Ann";
-			// mixedArray[2] = false;
-			// mixedArray[3] = DateTime.Now;
-			// mixedArray[4] = 112.22;
-			
-			// Console.WriteLine("---Printing array elements and their types---");
-			// for(int i = 0; i < mixedArray.Length; i++) {
-			//     Console.WriteLine("Value-> {0} :: Type-> {1}", mixedArray[i], mixedArray[i].GetType());
-			// }
-			
-			// string[] newNums = program.Test(mixedArray);
-			// Console.WriteLine("\n---Printing array elements and their types---");
-			// for(int i = 0; i < newNums.Length; i++) {
-			//     Console.WriteLine("Value-> {0} :: Type-> {1}", newNums[i], newNums[i].GetType());
-			// }
+		// 	Console.Write("Input an integer value:");
+		// 	int i = Convert.ToInt32(Console.ReadLine());
+		// 	Console.WriteLine("Check whether the said value is greater than is swap value: {0}", program.Test(i));
 		}
 		
-		// public string[] Test(object[] nums) {
-		//     return Array.ConvertAll(nums, x => x.ToString());
+		// public bool Test(int i) {
+		// 	return (int)(i / 10) >= i % 10;
 		// }
     }
 }
